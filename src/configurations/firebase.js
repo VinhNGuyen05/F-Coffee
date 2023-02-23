e3c72a551a2f3a51dacdf29ec48fae242ff069bf
vinh nguyen <vinhnhse151180@fpt.edu.vn>
2023-02-23 08:45:50 +0700

    set up firebase deploy

---
+++ src/configurations/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEypg-ykY_lrKewFHBxyYPS8-otIF0lBI",
+  authDomain: "fv-coffee.firebaseapp.com",
+  projectId: "fv-coffee",
+  storageBucket: "fv-coffee.appspot.com",
+  messagingSenderId: "1090726616271",
+  appId: "1:1090726616271:web:4a87a8d791b1c87fa12bba"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
